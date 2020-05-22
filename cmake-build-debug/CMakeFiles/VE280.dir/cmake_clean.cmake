67c17cf7ad4f657fb5667f963226fc1331885798
Yu Xinmiao <yuxinmiao@sjtu.edu.cn>
2020-05-22 09:18:01 +0800

    no message

---
+++ cmake-build-debug/CMakeFiles/VE280.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/VE280.dir/lecture_note.cpp.o"
+  "VE280"
+  "VE280.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/VE280.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
