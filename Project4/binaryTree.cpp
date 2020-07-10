ba80ca0760abf00e92ab38d7ee3b085146694534
Yu Xinmiao <yuxinmiao@sjtu.edu.cn>
2020-07-10 11:39:12 +0800

    P4 temporary update

--- Project4/binaryTree.cpp
+++ Project4/binaryTree.cpp
@@ -7,76 +7,144 @@
 using namespace std;
 
 /* ================================== Node =================================== */
-Node::Node(const std::string &str, int num, Node *left, Node *right) {
-    // TODO: implement this function.
-}
+Node::Node(const std::string &str, int num, Node *left, Node *right)
+: str(str), num(num), left(left), right(right)  {}
 
-Node* Node::leftSubtree() const{
-    // TODO: implement this function.  
-}
+Node* Node::leftSubtree() const{return left;}
 
-Node* Node::rightSubtree() const{
-    // TODO: implement this function.  
-}
+Node* Node::rightSubtree() const{return right;}
 
-string Node::getstr() const {
-    // TODO: implement this function.  
-}
+string Node::getstr() const {return str;}
 
-int Node::getnum() const {
-    // TODO: implement this function.  
-}
+int Node::getnum() const {return num;}
+
+void Node::setleft(Node *n) {left = n;}
+
+void Node::setright(Node *n) {right = n;}
+
+void Node::incnum() {num++;}
 
 Node *Node::mergeNodes(Node *leftNode, Node *rightNode) {
-    // TODO: implement this function.
+    return new Node(leftNode->str + rightNode->str, leftNode->num + rightNode->num, leftNode, rightNode);
 }
 
 /* =============================== Binary Tree =============================== */
 
 BinaryTree::BinaryTree(Node *rootNode){
-    // TODO: implement this function.
+    root = rootNode;
 }
-
+void deleteNodes(Node* root) {
+    // helper function for freeing all the memory allocated in this binary tree.
+    if (root->leftSubtree())
+        deleteNodes(root->leftSubtree());
+    if (root->rightSubtree())
+        deleteNodes(root->rightSubtree());
+    delete root;
+}
 BinaryTree::~BinaryTree() {
-    // TODO: implement this function.
+    if (root) deleteNodes(root);
+    else delete root;
 }
 
+string findPathHelper(Node* root, string &path, const string& s) {
+
+    if (root->getstr() == s) return path;
+    if (!root->leftSubtree() && !root->rightSubtree()) return "x";
+    if (root->leftSubtree()) {return findPathHelper(root->leftSubtree(), path, s) == path ? path += "0" : path;}
+    if (root->rightSubtree()) {return findPathHelper(root->rightSubtree(), path, s) == path ? path += "1": path;}
+    return path;
+}
+/*
+bool findPathHelper2(Node* root, string &path, const string& s) {
+    static bool exist = false;
+    if (root->getstr() == s) return true;
+    if (!root->leftSubtree() && !root->rightSubtree()) return false;
+    if (root->leftSubtree()) {if(findPathHelper2(root->leftSubtree(), path, s)) {path += "0";exist = true;}}
+    if (root->rightSubtree() && !exist) {if(findPathHelper2(root->leftSubtree(), path, s)) path += "1";}
+
+}*/
+bool findPathHelper3(Node* root, string &path, const string& s) {
+    if (root->getstr() == s) return true;
+    if (!root->leftSubtree() && !root->rightSubtree()) return false;
+    if (root->leftSubtree()) {if(findPathHelper3(root->leftSubtree(), path, s)) path += "0";}
+    if (root->rightSubtree()) {if(findPathHelper3(root->leftSubtree(), path, s)) path += "1";}
+    return true;
+}
 string BinaryTree::findPath(const string &s) const{
-    // TODO: implement this function.
+    if (root->getstr() == s) return "";
+    string path;
+    findPathHelper(root, path, s);
+    if (path.empty()) return "-1";
+    else return path;
+}
+
+int sumHelper (Node* root) { // helper function for sum
+    if (!root) return 0;
+    return root->getnum() + sumHelper(root->leftSubtree()) + sumHelper(root->rightSubtree());
 }
 
 int BinaryTree::sum() const {
-    // TODO: implement this function.
+    return sumHelper(root);
 }
 
+int depthHelper(Node* root) { // helper function for depth
+    if (!root) return 0;
+    int leftDepth = depthHelper(root->leftSubtree());
+    int rightDepth = depthHelper(root->rightSubtree());
+    if (leftDepth > rightDepth) return leftDepth + 1;
+    else return rightDepth + 1;
+}
 int BinaryTree::depth() const {
-    // TODO: implement this function.
+    return depthHelper(root);
 }
 
+void preorderNumHelper(Node* root) {
+    if (!root) return;
+    cout << root->getnum() << " ";
+    preorderNumHelper(root->leftSubtree());
+    preorderNumHelper(root->rightSubtree());
+}
 void BinaryTree::preorder_num() const {
-    // TODO: implement this function.
+    preorderNumHelper(root);
 }
 
+void inorderStrHelper(Node* root) {
+    if (!root) return;
+    inorderStrHelper(root->leftSubtree());
+    cout << root->getstr() << " ";
+    inorderStrHelper(root->rightSubtree());
+}
 void BinaryTree::inorder_str() const {
-    // TODO: implement this function.
+    inorderStrHelper(root);
 }
 
+void postorderNumHlper(Node* root) {
+    if (!root) return;
+    postorderNumHlper(root->leftSubtree());
+    postorderNumHlper(root->rightSubtree());
+    cout << root->getnum() << " ";
+}
 void BinaryTree::postorder_num() const {
-    // TODO: implement this function.
+    postorderNumHlper(root);
 }
 
 bool BinaryTree::allPathSumGreater(int temp) const {
     // TODO: implement this function.
+    return false;
 }
 
 bool BinaryTree::covered_by(const BinaryTree &tree) const {
     // TODO: implement this function.
+    return false;
 }
 
 bool BinaryTree::contained_by(const BinaryTree &tree) const {
     // TODO: implement this function.
+    return false;
 }
 
 BinaryTree BinaryTree::copy() const {
     // TODO: implement this function.
+    return *this;
 }
+
--- Project4/main.cpp
+++ Project4/main.cpp
@@ -1,6 +1,46 @@
 #include "binaryTree.h"
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <sstream>
+
+using namespace std;
+
 
 int main(int argc, char *argv[]) {
+    Node* N1 = new Node("a", 3);
+    Node* N2 = new Node("b",6);
+    Node* N3 = new Node("c", 9);
+    Node* N4 = new Node("d", 10);
+    Node* N5 = new Node("e", 13);
+    Node* N6 = new Node("f", 2, N4, N5);
+    Node* N7 = new Node("g",18);
+    Node* N8 = new Node("h",4);
+    N1->setleft(N2);
+    N1->setright(N3);
+    N3->setright(N6);
+    N2->setright(N7);
+    N2->setleft(N8);
+    BinaryTree emptyTree(nullptr);
+    BinaryTree Tree1(N1);
 
+    Node* D1 = new Node("ab", 9);
+    BinaryTree Tree2(D1);
+
+    Node* A1 = new Node("xy", 10);
+    Node* A2 = new Node("uu", 11);
+    A1->setleft(A2);
+    BinaryTree Tree3(A1);
+
+    Node* B2 = new Node("ku", 9);
+    Node* B1 = new Node("mm", 7, nullptr, B2);
+    BinaryTree Tree4(B1);
+   /* cout << emptyTree.depth()  <<" Tree1:" << Tree1.depth() << " Tree2:" << Tree2.depth() << " Tree3:"
+    << Tree3.depth() << " Tree4:" << Tree4.depth() << endl;*/
+    emptyTree.postorder_num();
+    Tree1.postorder_num();
+    Tree2.postorder_num();
+    Tree3.postorder_num();
+    Tree4.postorder_num();
     return 0;
 }
